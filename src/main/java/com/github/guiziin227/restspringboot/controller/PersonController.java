1de4650a66dbc1f6f5253bc2457537661b051756
Guiziin227 <gh63388@gmail.com>
2025-05-23 19:33:21 -0300

    refactor: update delete endpoint in PersonController to return no content response

--- src/main/java/com/github/guiziin227/restspringboot/controller/PersonController.java
+++ src/main/java/com/github/guiziin227/restspringboot/controller/PersonController.java
@@ -4,6 +4,7 @@ import com.github.guiziin227.restspringboot.model.Person;
 import com.github.guiziin227.restspringboot.service.PersonService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -46,8 +47,9 @@ public class PersonController {
     }
 
     @DeleteMapping(value = "/{id}")
-    public void delete(@PathVariable("id") Long id) {
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
         personService.delete(id);
+        return ResponseEntity.noContent().build();
     }
 
 }
