948278a59872312217268443877062acd78a22e1
Guiziin227 <gh63388@gmail.com>
2025-05-25 18:50:38 -0300

    feat: add SQL scripts for creating and populating the person table

---
+++ src/main/resources/db/migration/V2__Populate_Table_Person.sql
@@ -0,0 +1,4 @@
+INSERT INTO `person` (`id`, `address`, `first_name`, `gender`, `last_name`)
+VALUES (2, 'Santa Maria - RS', 'Maria Luiza', 'Feminino', 'Leite'),
+       (3, 'Santa Maria - RS', 'Guilherme', 'Masculino', 'Henriques');
+
