0495963eb75dd618337ec9ff5afd7c2b457b5143
Guiziin227 <gh63388@gmail.com>
2025-05-26 15:01:31 -0300

    feat: add YamlJackson2HttpMessageConverter for YAML support in content negotiation

---
+++ src/main/java/com/github/guiziin227/restspringboot/serializer/converter/YamlJackson2HttpMessageConverter.java
@@ -0,0 +1,16 @@
+package com.github.guiziin227.restspringboot.serializer.converter;
+
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.json.AbstractJackson2HttpMessageConverter;
+
+public final class YamlJackson2HttpMessageConverter extends AbstractJackson2HttpMessageConverter {
+
+    protected YamlJackson2HttpMessageConverter() {
+        super(new YAMLMapper()
+                .setSerializationInclusion(JsonInclude.Include.NON_NULL),
+                MediaType.parseMediaType("application/x-yaml; charset=UTF-8")
+        );
+    }
+}
