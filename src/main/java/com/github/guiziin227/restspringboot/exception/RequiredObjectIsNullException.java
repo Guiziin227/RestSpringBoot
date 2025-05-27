b777a9262c96432408044db500007ac6267062d5
Guiziin227 <gh63388@gmail.com>
2025-05-27 19:41:53 -0300

    feat: add exception handler for RequiredObjectIsNullException in CustomEntityResponseHandler

---
+++ src/main/java/com/github/guiziin227/restspringboot/exception/RequiredObjectIsNullException.java
@@ -0,0 +1,12 @@
+package com.github.guiziin227.restspringboot.exception;
+
+public class RequiredObjectIsNullException extends RuntimeException {
+
+    public RequiredObjectIsNullException() {
+        super("It is not allowed to persist a null object!");
+    }
+
+    public RequiredObjectIsNullException(String message) {
+        super(message);
+    }
+}
--- src/main/java/com/github/guiziin227/restspringboot/exception/handler/CustomEntityResponseHandler.java
+++ src/main/java/com/github/guiziin227/restspringboot/exception/handler/CustomEntityResponseHandler.java
@@ -1,6 +1,7 @@
 package com.github.guiziin227.restspringboot.exception.handler;
 
 import com.github.guiziin227.restspringboot.exception.ExceptionResponse;
+import com.github.guiziin227.restspringboot.exception.RequiredObjectIsNullException;
 import com.github.guiziin227.restspringboot.exception.ResourceNotFoundException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -34,5 +35,15 @@ public class CustomEntityResponseHandler extends ResponseEntityExceptionHandler 
                 request.getDescription(false)
         );
         return new ResponseEntity<>(resp, HttpStatus.NOT_FOUND);
+    }
+
+
+    @ExceptionHandler(RequiredObjectIsNullException.class)
+    public final ResponseEntity<ExceptionResponse> handleBadRequestExceptions(Exception ex, WebRequest request) {
+        ExceptionResponse response = new ExceptionResponse(
+                new Date(),
+                ex.getMessage(),
+                request.getDescription(false));
+        return new ResponseEntity<>(response, HttpStatus.BAD_REQUEST);
     }
 }
