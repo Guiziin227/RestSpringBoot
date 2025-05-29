9da20a96eed4908cf018a4f82b0e09621cc15e9b
Guiziin227 <gh63388@gmail.com>
2025-05-29 18:29:24 -0300

    feat: implement BookController, BookService, and BookRepository for book management

---
+++ src/main/java/com/github/guiziin227/restspringboot/controller/BookController.java
@@ -0,0 +1,83 @@
+package com.github.guiziin227.restspringboot.controller;
+
+import com.github.guiziin227.restspringboot.dto.BookDTO;
+import com.github.guiziin227.restspringboot.service.BookService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/book/v1")
+public class BookController {
+
+    private final BookService bookService;
+
+    @GetMapping(
+            produces = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE
+            })
+    public List<BookDTO> findAll() {
+        return bookService.findAll();
+    }
+
+    @GetMapping(value = "/{id}",
+            produces = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE
+            })
+    public BookDTO findById(@PathVariable("id") Long id) {
+        return bookService.findById(id);
+    }
+
+
+    @PostMapping(
+            consumes = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE
+            },
+            produces = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE
+            }
+    )
+    public BookDTO create(@RequestBody BookDTO bookDTO) {
+        return bookService.create(bookDTO);
+    }
+
+    @PutMapping(
+            consumes = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE},
+            produces = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE
+            })
+    public BookDTO update(@RequestBody BookDTO bookDTO) {
+        return bookService.update(bookDTO);
+    }
+
+    @DeleteMapping(value = "/{id}")
+    public ResponseEntity<?> delete(@PathVariable("id") Long id) {
+        bookService.delete(id);
+        return ResponseEntity.noContent().build();
+    }
+
+}
