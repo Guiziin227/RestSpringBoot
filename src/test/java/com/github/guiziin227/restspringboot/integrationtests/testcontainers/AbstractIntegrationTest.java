f8e20e499f34a49be14c3ac9374256e15586a8af
Guiziin227 <gh63388@gmail.com>
2025-06-02 18:11:59 -0300

    feat: add integration test setup with MySQL container using Testcontainers

---
+++ src/test/java/com/github/guiziin227/restspringboot/integrationtests/testcontainers/AbstractIntegrationTest.java
@@ -0,0 +1,45 @@
+package com.github.guiziin227.restspringboot.integrationtests.testcontainers;
+
+import org.springframework.context.ApplicationContextInitializer;
+import org.springframework.context.ConfigurableApplicationContext;
+import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.core.env.MapPropertySource;
+import org.springframework.test.context.ContextConfiguration;
+import org.testcontainers.containers.MySQLContainer;
+import org.testcontainers.lifecycle.Startables;
+
+import java.util.Map;
+import java.util.stream.Stream;
+
+@ContextConfiguration(initializers = AbstractIntegrationTest.Initializer.class)
+public class AbstractIntegrationTest {
+
+    public static class Initializer implements ApplicationContextInitializer<ConfigurableApplicationContext> {
+
+        static MySQLContainer<?> mysql = new MySQLContainer<>("docker pull mysql:9.1.0");
+
+        private static void startContainers() {
+            Startables.deepStart(Stream.of(mysql)).join();
+        }
+
+        @Override
+        public void initialize(ConfigurableApplicationContext applicationContext) {
+            startContainers();
+            ConfigurableEnvironment environment = applicationContext.getEnvironment();
+            MapPropertySource testcontainers = new MapPropertySource("testcontainers",
+                    (Map) createConnectionConfiguration());
+            environment.getPropertySources().addFirst(testcontainers);
+        }
+
+        private Map<String, String> createConnectionConfiguration() {
+            return Map.of(
+                    "spring.datasource.url", mysql.getJdbcUrl(),
+                    "spring.datasource.username", mysql.getUsername(),
+                    "spring.datasource.password", mysql.getPassword(),
+                    "spring.datasource.driver-class-name", mysql.getDriverClassName()
+            );
+        }
+
+
+    }
+}
