a02451053875a9135bb35f463333a2a57a56bd07
Guiziin227 <gh63388@gmail.com>
2025-06-03 20:00:45 -0300

    feat: add CORS support configuration to WebConfig

--- src/main/java/com/github/guiziin227/restspringboot/config/WebConfig.java
+++ src/main/java/com/github/guiziin227/restspringboot/config/WebConfig.java
@@ -1,12 +1,26 @@
 package com.github.guiziin227.restspringboot.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.MediaType;
 import org.springframework.web.servlet.config.annotation.ContentNegotiationConfigurer;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
+
+    @Value("${cors.originPatterns:default}")
+    private String corsOriginPattern = "";
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+      var allowedOrigins = corsOriginPattern.split(",");
+        registry.addMapping("/**")
+                .allowedOrigins(allowedOrigins)
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS")
+                .allowCredentials(true);
+    }
 
     @Override
     public void configureContentNegotiation(ContentNegotiationConfigurer configurer) {
