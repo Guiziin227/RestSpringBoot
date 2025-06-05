f3f1e179887bc23ee42dc30a19ab869e8f927324
Guiziin227 <gh63388@gmail.com>
2025-06-05 14:53:01 -0300

    feat: add authorization and origin header parameters to TestConfigs interface

--- src/test/java/com/github/guiziin227/restspringboot/config/TestConfigs.java
+++ src/test/java/com/github/guiziin227/restspringboot/config/TestConfigs.java
@@ -2,4 +2,10 @@ package com.github.guiziin227.restspringboot.config;
 
 public interface TestConfigs {
     int SERVER_PORT = 8888;
+
+    String HEADER_PARAM_AUTHORIZATION = "Authorization";
+    String HEADER_PARAM_ORIGIN = "Origin";
+
+    String ORIGIN_LOCALHOST = "http://localhost:4200";
+    String ORIGIN_ERRADA = "http://localhost:4201";
 }
