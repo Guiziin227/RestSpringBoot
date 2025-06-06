4eb0319f9fb7746cfe00f48df911060e651ef03d
Guiziin227 <gh63388@gmail.com>
2025-06-06 18:35:50 -0300

    feat: add disablePerson endpoint to PersonController and update service links

--- src/main/java/com/github/guiziin227/restspringboot/controller/PersonController.java
+++ src/main/java/com/github/guiziin227/restspringboot/controller/PersonController.java
@@ -9,6 +9,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -93,6 +94,17 @@ public class PersonController implements com.github.guiziin227.restspringboot.co
     @Override
     public PersonDTO update(@RequestBody PersonDTO person) {
         return personService.update(person);
+    }
+
+    @PatchMapping(value = "/{id}",
+            produces = {
+                    MediaType.APPLICATION_JSON_VALUE,
+                    MediaType.APPLICATION_XML_VALUE,
+                    MediaType.APPLICATION_YAML_VALUE
+            })
+    @Override
+    public PersonDTO disablePerson(@PathVariable("id") Long id) {
+        return personService.disablePerson(id);
     }
 
 }
