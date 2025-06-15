04d8bacb8f0d2c71bf590d767346d99863b1b89c
Guiziin227 <gh63388@gmail.com>
2025-06-15 15:21:40 -0300

    feat: add loadFileAsResource method to FileStorageService for file retrieval

--- src/main/java/com/github/guiziin227/restspringboot/service/FileStorageService.java
+++ src/main/java/com/github/guiziin227/restspringboot/service/FileStorageService.java
@@ -2,10 +2,13 @@ package com.github.guiziin227.restspringboot.service;
 
 import com.github.guiziin227.restspringboot.config.FileStorageConfig;
 import com.github.guiziin227.restspringboot.controller.FileController;
+import com.github.guiziin227.restspringboot.exception.FileNotFoundException;
 import com.github.guiziin227.restspringboot.exception.FileStorageException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.UrlResource;
 import org.springframework.stereotype.Service;
 import org.springframework.util.StringUtils;
 import org.springframework.web.multipart.MultipartFile;
@@ -49,6 +52,26 @@ public class FileStorageService {
         }catch (Exception e) {
             logger.error("Could not store file {}. Error: {}", fileName, e.getMessage(), e);
             throw new FileStorageException("Could not store file " + fileName + ". Please try again!", e);
+        }
+    }
+
+    public Resource loadFileAsResource(String fileName) {
+        try {
+
+            Path filePath = this.fileStorageLocation.resolve(fileName).normalize();
+
+            Resource resource = new UrlResource(filePath.toUri());
+
+            if(resource.exists() || resource.isReadable()) {
+                return resource;
+            } else {
+                logger.error("File {} not found or not readable", fileName);
+                throw new FileNotFoundException("File " + fileName + " not found or not readable");
+            }
+
+        } catch (Exception e) {
+            logger.error("Could not load file {}. Error: {}", fileName, e.getMessage(), e);
+            throw new FileNotFoundException("Could not load file " + fileName + ". Please try again!", e);
         }
     }
 }
